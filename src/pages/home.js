98f049c00e187fc8626483d4853e2fbe23bc68d6
yodyibram <yody.theblues@gmail.com>
2023-08-29 18:30:59 +0700

    first commit

---
+++ src/pages/home.js
@@ -0,0 +1,165 @@
+import React from 'react';
+
+class Home extends React.Component{
+    render(){
+        return(
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src="https://images.glints.com/unsafe/1920x0/glints-dashboard.s3.amazonaws.com/company-logo/d230e2748d2c63db0a4033253ce96356.png" width="200"/>
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto d-flex gap-4">
+              <li class="nav-item">
+                <a href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a href="#">Kelas</a>
+              </li>
+              <li class="nav-item">
+                <a href="#">Hubungi Admin</a>
+              </li>
+              <li class="nav-item">
+                <a href="#">Masuk</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <section id="hero-section">
+        <div class="container">
+            <div class="row">
+                <h1 class="display-4 text-white">
+                    Raih profesi idamanmu,  <br/> 
+                    kami bantu sampai diterima kerja
+                </h1>
+                <p class="text-white opacity-75">
+                    Tanpa biaya pendaftaran
+                </p>
+                <div class="d-flex gap-3 mt-2 mt-lg-4">
+                    <a href="#" class="btn bg-white text-black rounded-pill px-4 shadow">
+                        Daftar Sekarang
+                    </a>
+                </div>
+            </div>
+        </div>
+      </section>
+    
+      <section id="values-products">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="d-flex flex-row flex-lg-column">
+                        <img src="1.jpg" class="col-4 rounded-4" />
+                    </div>
+                    <div class="d-flex flex-column col-8 ms-3 ms-lg-0 detail mt-3-md-0">
+                        <h5 class="mt-lg-3">Title</h5>
+                        <p class="mt-lg-2">Maxime mollitia,
+                            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                            optio, eaque rerum! Provident similique accusantium nemo autem</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="d-flex flex-row flex-lg-column">
+                        <img src="1.jpg" class="col-4 rounded-4" />
+                    </div>
+                    <div class="d-flex flex-column col-8 ms-3 ms-lg-0 detail mt-3-md-0">
+                        <h5 class="mt-lg-3">Title</h5>
+                        <p class="mt-lg-2">Maxime mollitia,
+                            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                            optio, eaque rerum! Provident similique accusantium nemo autem</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="d-flex flex-row flex-lg-column">
+                        <img src="1.jpg" class="col-4 rounded-4" />
+                    </div>
+                    <div class="d-flex flex-column col-8 ms-3 ms-lg-0 detail mt-3-md-0">
+                        <h5 class="mt-lg-3">Title</h5>
+                        <p class="mt-lg-2">Maxime mollitia,
+                            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                            optio, eaque rerum! Provident similique accusantium nemo autem</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="d-flex flex-row flex-lg-column">
+                        <img src="1.jpg" class="col-4 rounded-4" />
+                    </div>
+                    <div class="d-flex flex-column col-8 ms-3 ms-lg-0 detail mt-3-md-0">
+                        <h5 class="mt-lg-3">Title</h5>
+                        <p class="mt-lg-2">Maxime mollitia,
+                            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                            optio, eaque rerum! Provident similique accusantium nemo autem</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="d-flex flex-row flex-lg-column">
+                        <img src="1.jpg" class="col-4 rounded-4" />
+                    </div>
+                    <div class="d-flex flex-column col-8 ms-3 ms-lg-0 detail mt-3-md-0">
+                        <h5 class="mt-lg-3">Title</h5>
+                        <p class="mt-lg-2">Maxime mollitia,
+                            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                            optio, eaque rerum! Provident similique accusantium nemo autem</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="d-flex flex-row flex-lg-column">
+                        <img src="1.jpg" class="col-4 rounded-4" />
+                    </div>
+                    <div class="d-flex flex-column col-8 ms-3 ms-lg-0 detail mt-3-md-0">
+                        <h5 class="mt-lg-3">Title</h5>
+                        <p class="mt-lg-2">Maxime mollitia,
+                            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                            optio, eaque rerum! Provident similique accusantium nemo autem</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="d-flex flex-row flex-lg-column">
+                        <img src="1.jpg" class="col-4 rounded-4" />
+                    </div>
+                    <div class="d-flex flex-column col-8 ms-3 ms-lg-0 detail mt-3-md-0">
+                        <h5 class="mt-lg-3">Title</h5>
+                        <p class="mt-lg-2">Maxime mollitia,
+                            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                            optio, eaque rerum! Provident similique accusantium nemo autem</p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6 col-lg-3">
+                    <div class="d-flex flex-row flex-lg-column">
+                        <img src="1.jpg" class="col-4 rounded-4" />
+                    </div>
+                    <div class="d-flex flex-column col-8 ms-3 ms-lg-0 detail mt-3-md-0">
+                        <h5 class="mt-lg-3">Title</h5>
+                        <p class="mt-lg-2">Maxime mollitia,
+                            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                            numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                            optio, eaque rerum! Provident similique accusantium nemo autem</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+        </div>
+        );
+    }
+}
+
+export default Home;
